da98979cde9042531dc5791db77402c61452fb09
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-14 17:26:42 +0700

    Membuat file variabel.cpp

---
+++ 07. Variabel/variabel.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int x = 5; //Ini merupakan cara untuk inisialisasi variabel bertipe integer
+	cout << "Nilai dari x adalah " << x << endl; //Ini merupakan cara untuk memanggil variabel ke console
+	cin.get();
+}
