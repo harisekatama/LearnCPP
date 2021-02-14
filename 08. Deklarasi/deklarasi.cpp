9e9552c2039f80ad03311c7bc4e0052c456d4050
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-14 17:36:02 +0700

    Membuat file deklarasi.cpp

---
+++ 08. Deklarasi/deklarasi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	int a; //Cara deklarasi variabel
+	a = 10; //Memasukkan nilai 10 ke variabel a
+	cout << "Nilai dari a : " << a << endl;
+
+	int b;
+	cout << "Masukkan nilai b : ";
+	cin >> b;
+	cout << "Nilai dari b : " << b << endl;
+
+	cin.get();
+}
