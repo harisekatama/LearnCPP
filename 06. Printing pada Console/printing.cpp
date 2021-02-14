3ec8dc581380d8aad7100fd38e0795ce3c13f63a
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-14 17:19:56 +0700

    Membuat program printing

---
+++ 06. Printing pada Console/printing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Hallo Semuaanya\n"; // \n merupakan shortcut untuk new line sama seperti (std::endl)
+	cout << "Ini merupakan baris baru.";
+	cout << "Tapi ini bukan baris baru.";
+	cout << " Dan ini adalah akhir dari kalimat.";
+	cin.get();
+}
