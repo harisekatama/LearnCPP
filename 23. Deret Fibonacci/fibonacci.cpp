d9207c0e078c20bba16935f441942297948ab80d
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-18 22:59:26 +0700

    Membuat fibonacci.cpp

---
+++ 23. Deret Fibonacci/fibonacci.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*
+		fn1 = f(n-1);
+		fn2 = f(n-2);
+	*/
+
+	int n, fn1, fn2, hasil;
+	fn1 = 0;
+	fn2 = 1;
+
+	cout << "DERET FIBONACCI !\n";
+	cout << "Masukkan nilai n : ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		hasil = fn1 + fn2;
+		fn2 = fn1;
+		fn1 = hasil;
+	}
+
+	cout << "\nDeret Fibonacci ke " << n << " adalah " << hasil << endl;
+
+	cin.get();
+	return 0;
+}
