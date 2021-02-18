cca0d430330ad048e7a559e5bba701d3f4b48d83
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-18 23:41:45 +0700

    Membuat patternsiku.cpp

---
+++ 24. Pattern Segitiga Siku - Siku/patternsiku.cpp
@@ -0,0 +1,96 @@
+//Pattern Segitiga Siku Siku !
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "Masukkan nilai n : ";
+	cin >> n;
+
+	cout << "\nPola 1 : \n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+	cout << "\nPola 2 : \n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n; j >= i; j--)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+	cout << "\nPola 3 : \n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n; j > i; j--)
+		{
+			cout << "  ";
+		}
+		for (int k = 1; k <=i ; k++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+
+	cout << "\nPola 4 : \n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			cout << "  ";
+		}
+		for (int k = n; k >= i; k--)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+	cin.get();
+	return 0;
+}
+
+//CONTOH JIKA PROGRAM DIJALANKAN !
+/*
+Masukkan nilai n : 5
+
+Pola 1 :
+*
+* *
+* * *
+* * * *
+* * * * *
+
+Pola 2 :
+* * * * *
+* * * *
+* * *
+* *
+*
+
+Pola 3 :
+	    *
+    * *
+	  * * *
+  * * * *
+* * * * *
+
+Pola 4 :
+* * * * *
+  * * * *
+    * * *
+  * *
+    *
+*/
