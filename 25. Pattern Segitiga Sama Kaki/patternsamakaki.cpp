eba00c300555a92850e29b2d758234a1833dcaaf
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-19 22:08:00 +0700

    Major Update
    Menambahkan Pola 2, Pola 3, serta Komentar

--- 25. Pattern Segitiga Sama Kaki/patternsamakaki.cpp
+++ 25. Pattern Segitiga Sama Kaki/patternsamakaki.cpp
@@ -1,35 +1,33 @@
-//Pattern Segitiga Sama Kaki
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	int n;
-
+	cout << "PATTERN SEGITIGA SAMA KAKI !\n\n";
 	cout << "Masukkan nilai n : ";
 	cin >> n;
 
 	cout << "\nPola 1 : \n";
 	for (int i = 1; i <= n; i++)
 	{
-		for (int j = n; j >= i; j--)
+		for (int j = n; j > i; j--)
 		{
 			cout << "  ";
 		}
-		for (int k = 1; k <= (2*i-1); k++)
+		for (int k = 1; k <= (2 * i - 1); k++)
 		{
 			cout << "* ";
 		}
-		cout << "\n";
+		cout << endl;
 	}
 
-	/*
 	cout << "\nPola 2 : \n";
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j < i; j++)
 		{
-			cout << "* ";
+			cout << "  ";
 		}
 		for (int k = n; k >= (2 * i - n); k--)
 		{
@@ -37,8 +35,61 @@ int main()
 		}
 		cout << endl;
 	}
-	*/
 
-	cin.get();
-	return 0;
+	cout << "\nPola 3 : \n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n; j > i; j--)
+		{
+			cout << "  ";
+		}
+		for (int k = 1; k <= (2 * i - 1); k++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	n -= 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "  ";
+		}
+		for (int k = n; k >= (2 * i - n); k--)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
 }
+
+/*
+CONTOH OUTPUT PROGRAMNYA !
+
+Pola 1 :
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+Pola 2 :
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+
+Pola 3 :
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+*/
