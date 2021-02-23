a37d323c6c7811dea9f00fcd5908ebc522f9ad7c
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-23 23:47:04 +0700

    Menambahkan cara mencari ukuran dan jumlah member

--- 42. Pendahuluan Array/array.cpp
+++ 42. Pendahuluan Array/array.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	//CARA DEKLARASI ARRAY
 	int nilai[5];
 
@@ -12,11 +13,11 @@ int main()
 	nilai[2] = 3;
 	nilai[3] = 4;
 	nilai[4] = 5;
-
-	/*
+	*/
+	
 	//BISA JUGA SEPERTI INI !
 	int nilai[5] = { 1,2,3,4,5 };
-	*/
+	
 
 	cout << "Nilai Array ke 1 : " << nilai[0] << " || Address Array nilai[0] : " << &nilai[0] << endl;
 	cout << "Nilai Array ke 2 : " << nilai[1] << " || Address Array nilai[1] : " << &nilai[1] << endl;
@@ -24,10 +25,11 @@ int main()
 	cout << "Nilai Array ke 4 : " << nilai[3] << " || Address Array nilai[3] : " << &nilai[3] << endl;
 	cout << "Nilai Array ke 5 : " << nilai[4] << " || Address Array nilai[4] : " << &nilai[4] << endl;
 
-	//CARA MEMANIPULASI/MENGUBAH NILAI DARI ARRAY
+	//CARA MEMANIPULASI/MENGUBAH NILAI DARI ARRAY DENGAN POINTER
 	int* ptr = nilai;
 	*(ptr + 2) = 8;
 
+	//CARA MEMANIPULASI/MENGUBAH NILAI DARI ARRAY TANPA POINTER
 	nilai[3] = 9;
 
 	cout << "\nNilai Array ke 1 : " << nilai[0] << " || Address Array nilai[0] : " << &nilai[0] << endl;
@@ -35,6 +37,12 @@ int main()
 	cout << "Nilai Array ke 3 : " << nilai[2] << " || Address Array nilai[2] : " << &nilai[2] << endl;
 	cout << "Nilai Array ke 4 : " << nilai[3] << " || Address Array nilai[3] : " << &nilai[3] << endl;
 	cout << "Nilai Array ke 5 : " << nilai[4] << " || Address Array nilai[4] : " << &nilai[4] << endl;
+
+	//CARA MENCARI UKURAN DARI ARRAY
+	cout << "\nUkuran dari array        : " << sizeof(nilai) << " Byte" << endl;
+
+	//CARA MENCARI JUMLAH MEMBER DARI ARRAY
+	cout << "Jumlah member dari array : " << sizeof(nilai) / sizeof(int) << " Buah" << endl;
 
 	cin.get();
 	return 0;
