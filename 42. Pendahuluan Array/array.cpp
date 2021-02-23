2b1530163190cd2954e89d10cf1a382b879c2a19
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-23 23:38:55 +0700

    Membuat array.cpp

---
+++ 42. Pendahuluan Array/array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//CARA DEKLARASI ARRAY
+	int nilai[5];
+
+	//CARA INISIALISASI ARRAY
+	nilai[0] = 1;
+	nilai[1] = 2;
+	nilai[2] = 3;
+	nilai[3] = 4;
+	nilai[4] = 5;
+
+	/*
+	//BISA JUGA SEPERTI INI !
+	int nilai[5] = { 1,2,3,4,5 };
+	*/
+
+	cout << "Nilai Array ke 1 : " << nilai[0] << " || Address Array nilai[0] : " << &nilai[0] << endl;
+	cout << "Nilai Array ke 2 : " << nilai[1] << " || Address Array nilai[1] : " << &nilai[1] << endl;
+	cout << "Nilai Array ke 3 : " << nilai[2] << " || Address Array nilai[2] : " << &nilai[2] << endl;
+	cout << "Nilai Array ke 4 : " << nilai[3] << " || Address Array nilai[3] : " << &nilai[3] << endl;
+	cout << "Nilai Array ke 5 : " << nilai[4] << " || Address Array nilai[4] : " << &nilai[4] << endl;
+
+	//CARA MEMANIPULASI/MENGUBAH NILAI DARI ARRAY
+	int* ptr = nilai;
+	*(ptr + 2) = 8;
+
+	nilai[3] = 9;
+
+	cout << "\nNilai Array ke 1 : " << nilai[0] << " || Address Array nilai[0] : " << &nilai[0] << endl;
+	cout << "Nilai Array ke 2 : " << nilai[1] << " || Address Array nilai[1] : " << &nilai[1] << endl;
+	cout << "Nilai Array ke 3 : " << nilai[2] << " || Address Array nilai[2] : " << &nilai[2] << endl;
+	cout << "Nilai Array ke 4 : " << nilai[3] << " || Address Array nilai[3] : " << &nilai[3] << endl;
+	cout << "Nilai Array ke 5 : " << nilai[4] << " || Address Array nilai[4] : " << &nilai[4] << endl;
+
+	cin.get();
+	return 0;
+}
