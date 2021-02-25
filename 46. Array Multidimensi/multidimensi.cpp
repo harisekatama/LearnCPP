3b87de5e6a0287df7b9770f57bc27e90d8c5ae0f
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-25 23:47:57 +0700

    Major update
    menambahkan fungsi untuk print array dan print address array serta menambahkan prototipe

--- 46. Array Multidimensi/multidimensi.cpp
+++ 46. Array Multidimensi/multidimensi.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+void printArray(int*, int baris, int kolom);
+void addressArray(int*, int baris, int kolom);
+
 int main()
 {
 	/*
@@ -18,9 +21,14 @@ int main()
 	const int kolom = 2;
 	int matriks[baris][kolom] = { 2,3,5,7 };
 
-	cout << "MATRIKS\n";
+	printArray(*matriks, baris, kolom);
+
+	cout << "\nMATRIKS\n";
 	cout << "[ " << matriks[0][0] << " " << matriks[0][1] << " ]" << endl;
 	cout << "[ " << matriks[1][0] << " " << matriks[1][1] << " ]" << endl;
+
+	cout << endl;
+	addressArray(*matriks, baris, kolom);
 
 	cout << "\nADDRESS ARRAY : \n";
 	cout << "Address matriks[0][0] : " << &matriks[0][0] << endl;
@@ -28,14 +36,36 @@ int main()
 	cout << "Address matriks[1][0] : " << &matriks[1][0] << endl;
 	cout << "Address matriks[1][1] : " << &matriks[1][1] << endl;
 
-	cout << "\nADDRESS ARRAY : \n";
+	cin.get();
+	return 0;
+}
+
+void printArray(int* ptrMatriks, int baris, int kolom)
+{
+	int index = 0;
+	cout << "MATRIKS DENGAN FUNGSI\n";
+	for (int i = 0; i < baris; i++)
+	{
+		cout << "[ ";
+		for (int j = 0; j < kolom; j++)
+		{
+			cout << *(ptrMatriks + index) << " ";
+			index++;
+		}
+		cout << "]\n";
+	}
+}
+
+void addressArray(int* ptrMatriks, int baris, int kolom)
+{
+	int index = 0;
+	cout << "\nADDRESS ARRAY DENGAN FUNGSI\n";
 	for (int i = 0; i < baris; i++)
 	{
 		for (int j = 0; j < kolom; j++)
 		{
-			cout << "Address matriks[" << i << "][" << j << "] : " << &matriks[i][j] << endl;
+			cout << "Address matriks[" << i << "][" << j << "] : " << (ptrMatriks + index) << endl;
+			index++;
 		}
 	}
-	cin.get();
-	return 0;
 }
