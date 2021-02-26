a0aaa88013562fb30b58d4c8866d0a9c4f71015c
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-02-27 00:10:23 +0700

    Membuat sortstd.cpp

---
+++ 48. Sort Array Dengan Standard Library/sortstd.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<array>
+#include<algorithm>
+using namespace std;
+
+const size_t arraySize = 10;
+
+/*
+void nama_fungsi(array<int, ukuran_array> nama_array)
+{
+	statement;
+	for (jenis_variabel nama_variabel : nama_array)
+	{
+		statement;
+	}
+	statement;
+}
+*/
+
+void printArray(array<int, arraySize> angka)
+{
+		cout << "Array : ";
+		for (int a : angka)
+		{
+			cout << a << " ";
+		}
+		cout << "\n";
+}
+
+void printArray(array<char, arraySize> huruf)
+{
+	cout << "Array : ";
+	for (char a : huruf)
+	{
+		cout << a << " ";
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	array<int, arraySize>angka = { 5,4,6,3,7,2,8,1,9,0 };
+	array<char, arraySize>huruf = { 'q','w','e','r','t','y','u','i','o','p' };
+
+	cout << "SEBELUM DI SORT !\n";
+	printArray(angka);
+	printArray(huruf);
+
+	sort(angka.begin(), angka.end());
+	sort(huruf.begin(), huruf.end());
+	
+	cout << "\nSETELAH DI SORT !\n";
+	printArray(angka);
+	printArray(huruf);
+
+	cin.get();
+	return 0;
+}
