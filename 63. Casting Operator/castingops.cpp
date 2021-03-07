58af5096c4521606193f0df8fcbbf73d1e7732d1
Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2021-03-07 23:42:32 +0700

    Membuat castingops.cpp

---
+++ 63. Casting Operator/castingops.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	float b = 2.59f;
+	char c = 'd';
+
+	cout << a + (int)b << endl;
+	cout << (float)a + b << endl;
+	cout << (int)c << endl;
+
+	cin.get();
+	return 0;
+}
